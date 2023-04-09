6c4288facc2d6b3304982d16372cad10cb5e9b7f
beonezero <97246653+beonezero@users.noreply.github.com>
2023-04-09 18:31:00 +0300

    Create learning.js

---
+++ learning.js
@@ -0,0 +1,26 @@
+for (let sheep = 0; sheep < 10; sheep++) {
+  console.log("Посчитано овец: " + sheep + "!");
+}
+console.log("zzzZZZ");
+
+let timesToSayHello = 3;
+for (i = 0; i < timesToSayHello; i++) {
+  console.log("Привет !");
+}
+
+let animals = ["Лиса", "Енот", "Тигр", "Черепаха", "Панда", "Песец"];
+
+for (let i = 0; i < animals.length; i++) {
+  console.log("В этом зоопарке есть " + animals[i] + ".");
+}
+
+let name = "Yauheni";
+
+for (let i = 0; i < name.length; i++) {
+  console.log(name[i]);
+}
+
+var nam = "Ник";
+for (var i = 0; i < nam.length; i++) {
+  console.log("В моем имени есть буква " + nam[i] + ".");
+}
